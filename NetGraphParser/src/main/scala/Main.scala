554a16c40aae08dbdebca512518d56f6e1202e48
Sambhav Jain <sambhav.k.jain1611@gmail.com>
2023-10-11 07:04:29 -0500

    Add files via upload
    Added netgraphparser

---
+++ NetGraphParser/src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+import NetGraphAlgebraDefs.NetGraph
+import NetGraphAlgebraDefs.NetModelAlgebra.outputDirectory
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val graph2 = NetGraph.load("", outputDirectory)
+  }
+}
